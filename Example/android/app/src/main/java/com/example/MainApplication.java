d7558f26b9a62b25839a28113d93d0816e0d4115
Sanjana1508 <sanjana.podduturi@zemosolabs.com>
2021-10-01 10:57:13 +0530

    Video calling with twillio

--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.twiliorn.library.TwilioPackage;
 import com.facebook.soloader.SoLoader;
+import com.twiliorn.library.TwilioPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,7 +21,21 @@ public class MainApplication extends Application implements ReactApplication {
       return BuildConfig.DEBUG;
     }
 
-    @Override
+    // @Override
+    // protected List<ReactPackage> getPackages() {
+    //   @SuppressWarnings("UnnecessaryLocalVariable")
+    //   // List<ReactPackage> packages = new PackageList(this).getPackages();
+    //   // packages.add( new MainReactPackage());
+    //   // packages.add(new TwilioPackage());
+       
+    //   return Arrays.<ReactPackage>asList(
+    //       new MainReactPackage(),
+    //       new TwilioPackage()
+    //   );
+    //  // return packages;
+    // }
+
+     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
